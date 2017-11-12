db7cdcfe0a6796b59f65e4291dc7d4bdae2bd7b2
Shekhar Joshi <shekharanna1993@gmail.com>
2017-11-12 23:01:42 +0530

    Remove view paths, not required

--- app.js
+++ app.js
@@ -7,12 +7,8 @@ var bodyParser = require('body-parser');
 
 var dbs = require('./database/mysql');
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
